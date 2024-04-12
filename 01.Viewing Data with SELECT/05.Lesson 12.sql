8a0d65b6b8a9fe22e7b56332dd8644c81507a8be
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-12 12:40:47 +0300

    Lesson 12

---
+++ 01.Viewing Data with SELECT/05.Lesson 12.sql
@@ -0,0 +1,14 @@
+--Lesson 12
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE last_name LIKE 'Bu%";
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE last_name LIKE '%e%";
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE first_name LIKE 'Ann_";
