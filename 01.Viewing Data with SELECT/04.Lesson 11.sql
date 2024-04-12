9e0403dd2f9cd2907cca670a7ad1d902e8dfed72
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-12 12:40:35 +0300

    Lesson 11

---
+++ 01.Viewing Data with SELECT/04.Lesson 11.sql
@@ -0,0 +1,20 @@
+--Lesson 11
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE employee_id > 5;
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE employee_id >= 5;
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE employee_id < 8;
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE employee_id <= 8;
+
