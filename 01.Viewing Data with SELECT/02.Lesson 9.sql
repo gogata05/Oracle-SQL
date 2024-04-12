e53e3121f36b9ffe4e8f83fee779f30c13d9a148
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-12 11:03:59 +0300

    Lesson 9

---
+++ 01.Viewing Data with SELECT/02.Lesson 9.sql
@@ -0,0 +1,16 @@
+--Lesson 9
+SELECT employee_id, first_name, last_name
+FROM employee;
+
+
+SELECT employee_id, last_name, department_id
+FROM employee;
+
+
+SELECT employee_id, department_id, last_name
+FROM employee;
+
+
+SELECT department_id, department_name
+FROM department;
+
