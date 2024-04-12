c7a7dab0c6eb0fa8a07433f8e8a2ac352745b44e
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-12 11:04:25 +0300

    Lesson 10

---
+++ 01.Viewing Data with SELECT/03.Lesson 10.sql
@@ -0,0 +1,20 @@
+--Lesson 10
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE first_name = 'Jessica';
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE last_name = 'Smith';
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE last_name = 'Jones';
+
+
+SELECT employee_id, first_name, last_name
+FROM employee
+WHERE last_name <> 'Jones';
+
