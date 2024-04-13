3217ac8e9d8abff5e69a936655e864113fdf86fd
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-13 10:21:59 +0300

    lesson 22

---
+++ 03.Sorting Data/01.Lesson 22.sql
@@ -0,0 +1,38 @@
+--Lesson 22
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY last_name ASC;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY last_name;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY last_name DESC;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY salary ASC;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY salary DESC;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY hire_date DESC;
+
+
+SELECT employee_id, first_name, last_name, salary, hire_date
+FROM employee
+ORDER BY hire_date ASC;
