0a3698c0ff8b561a926015a6adbdf4800a9fac80
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-13 12:32:22 +0300

    lesson 30

---
+++ 05.Aggregate Functions and Grouping/01.Lesson 30.sql
@@ -0,0 +1,23 @@
+--Lesson 30
+SELECT COUNT(*)
+FROM employee;
+
+
+SELECT *
+FROM employee;
+
+
+SELECT COUNT(1)
+FROM employee;
+
+
+SELECT COUNT(*)
+FROM department;
+
+
+SELECT COUNT(*)
+FROM customer;
+
+
+SELECT COUNT(*)
+FROM customer_order;
