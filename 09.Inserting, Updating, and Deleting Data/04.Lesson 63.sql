8be3a29d0017412f69477cd770a5c800b24fd504
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-15 11:00:10 +0300

    lesson 63

---
+++ 09.Inserting, Updating, and Deleting Data/04.Lesson 63.sql
@@ -0,0 +1,31 @@
+--Lesson 63
+SELECT *
+FROM employee;
+
+
+SELECT *
+FROM employee
+WHERE employee_id = 250;
+
+
+DELETE FROM employee
+WHERE employee_id = 250;
+
+
+SELECT *
+FROM employee
+WHERE employee_id = 250;
+
+
+SELECT *
+FROM employee
+WHERE last_name = 'Larson';
+
+
+DELETE FROM employee
+WHERE last_name = 'Larson';
+
+
+SELECT *
+FROM employee
+WHERE last_name = 'Larson';
